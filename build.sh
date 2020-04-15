e46dc3aaba8b57cdca7aefbc0fa608799e473449
zyl <zyl@localhost.com>
2020-04-15 14:39:48 +0800

    f

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 operator-sdk build registry.ocp.zyl.io:5000/hello-operator:v1 \
    --image-builder podman
 podman push --authfile /root/pull-secret-2.json registry.ocp.zyl.io:5000/hello-operator:v1
+#podman push --authfile /root/pull-secret-2.json core.apps.ocp.zyl.io/hello/hello-operator:v1
